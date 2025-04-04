71b03dfce6fac1e963cf3fce21a012afc73958cb
Nic Raboy <nraboy@gmail.com>
2025-04-04 14:47:02 -0700

    Start Messing Around with User IDs

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-{
-  cd /app/server/ &&
-    npx prisma generate --schema=./prisma/schema.prisma &&
-    npx prisma migrate deploy --schema=./prisma/schema.prisma &&
-    node /app/server/index.js
-} &
-{ node /app/collector/index.js; } &
-wait -n
-exit $?
+
+if [ ! -z "${UID}" ]; then
+  if [ ! "$(id -u anythingllm)" -eq "${UID}" ]; then
+    usermod -o -u "${UID}" anythingllm
+  fi
+fi
+
+if [ ! -z "${GID}" ]; then
+  if [ ! "$(id -g anythingllm)" -eq "${GID}" ]; then
+    groupmod -g "${GID}" anythingllm
+  fi
+fi
+
+chown -R anythingllm:anythingllm /app
+chown -R anythingllm:anythingllm /app/server
+chown -R anythingllm:anythingllm /app/frontend
+chown -R anythingllm:anythingllm /app/collector
+chown -R anythingllm:anythingllm "${STORAGE_DIR}"
+
+exec gosu anythingllm "/usr/local/bin/docker-entrypoint-final.sh"
